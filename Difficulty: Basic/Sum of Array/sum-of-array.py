12ade63f45dc1ed1cbf176c9d4c1b52f4d4118ab
Rajan Kumar <101178067+iamrajank@users.noreply.github.com>
2025-01-22 18:12:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Array/sum-of-array.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+class Solution:
+
+    def arraySum(self,arr):
+           # code here
+           n = len(arr)
+           Sum = 0
+           i = 0
+           for i in range(n):
+               Sum = Sum + arr[i]
+           return Sum
+
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+# Driver code
+if __name__ == "__main__":
+    tc = int(input())
+    while tc > 0:
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.arraySum(arr)
+        print(ans)
+        tc = tc - 1
+        print("~")
+
+# } Driver Code Ends
