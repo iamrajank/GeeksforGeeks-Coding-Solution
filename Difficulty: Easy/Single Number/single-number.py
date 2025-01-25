086bb357d8e64a152f0a19bbd643e8942c6a586d
Rajan Kumar <101178067+iamrajank@users.noreply.github.com>
2025-01-25 20:38:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Single Number/single-number.py
@@ -0,0 +1,35 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    
+    def getSingle(self,arr):
+        # code here
+        count = 0
+        for i in range(len(arr)):
+            count = count ^ arr[i]
+        return count
+        
+
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        # k= int(input())
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.getSingle(arr)
+        print(res)
+        print("~")
+        t -= 1
+
+
+# } Driver Code Ends
