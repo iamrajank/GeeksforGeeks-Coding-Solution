68100e34fb802395a295632bf92cc271400f14f6
Rajan Kumar <101178067+iamrajank@users.noreply.github.com>
2025-02-14 21:28:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.py
@@ -0,0 +1,64 @@
+#User function Template for python3
+
+class Solution:
+    def countSubarrays(self, nums, k):
+        # code here
+        # count = 0
+        # for i in range(len(nums)):
+        #     ans = 0
+        #     for j in range(i,len(nums)):
+        #         ans = ans + nums[j]
+        #         if ans == k:
+        #             count = count + 1
+        # return count
+        
+        sub_num = {0:1}
+        total = count = 0
+
+        for n in nums:
+            total += n
+            
+            if total - k in sub_num:
+                count += sub_num[total-k]
+            
+            sub_num[total] = 1 + sub_num.get(total, 0)
+        
+        return count
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+from collections import deque
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        k = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        res = ob.countSubarrays(arr, k)
+        print(res)
+        print("~")
+
+# } Driver Code Ends
