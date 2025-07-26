e4980dac179e32c7eefbaa9942b87def90122df9
Rajan Kumar <101178067+iamrajank@users.noreply.github.com>
2025-07-27 00:33:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Binary Search/binary-search.py
+++ Difficulty: Easy/Binary Search/binary-search.py
@@ -1,37 +1,19 @@
-#User function template for Python
-
-class Solution:    
-    def binarysearch(self, arr, n):
-        # code here
-        s,e=0,len(arr)-1
-        while s<=e:
-            m=s+(e-s)//2
-            if arr[m]<k:
-                s=m+1
-            elif arr[m]==k:
-                if m==0 or arr[m-1]!=k:
-                    return m
-                e=m-1
+class Solution:
+    def binarysearch(self, arr, k):
+        # Code Here
+        n = len(arr)
+        l = 0
+        r = n - 1
+        result = -1
+        while l <= r:
+            mid = l + (r-l)//2
+            if arr[mid] == k:
+                result = mid
+                r = mid - 1
+            elif arr[mid] > k:
+                r = mid - 1
             else:
-                e=m-1
-        return -1
-
-
-
-
-
-#{ 
- # Driver Code Starts
-#Initial template for Python
-
-if __name__ == "__main__":
-    t = int(input())
-    for _ in range(t):
-        k = int(input())
-        arr = list(map(int, input().split()))
-        ob = Solution()
-        res = ob.binarysearch(arr, k)
-        print(res)
-        print("~")
-
-# } Driver Code Ends
+                l = mid + 1
+        return result
+        
+        
