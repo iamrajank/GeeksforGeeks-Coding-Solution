23fd8c66bcb72f3c37b39c7c46c6cf38c7466f0b
Rajan Kumar <101178067+iamrajank@users.noreply.github.com>
2025-07-28 23:17:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome/palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x):
+		# code here
+		reverse_digit = 0
+        x_copy = x
+        while x > 0:
+            digit = x % 10
+            reverse_digit = reverse_digit * 10 + digit
+            x = x // 10
+        
+        if x_copy == reverse_digit:
+            return True
+        return False
