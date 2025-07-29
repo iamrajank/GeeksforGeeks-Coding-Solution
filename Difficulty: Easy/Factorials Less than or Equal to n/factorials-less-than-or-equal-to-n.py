617f5d5278baea151db1e0279e842aeeaeda1450
Rajan Kumar <101178067+iamrajank@users.noreply.github.com>
2025-07-30 00:34:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Factorials Less than or Equal to n/factorials-less-than-or-equal-to-n.py
+++ Difficulty: Easy/Factorials Less than or Equal to n/factorials-less-than-or-equal-to-n.py
@@ -2,50 +2,47 @@
 
 class Solution:
     def factorialNumbers(self, n):
-        #code here
-        
-        # Brute Force Approach (TLE Error)
-        
+        #code here 
+    #     if n == 0:
+    #         return 1
+    #     else:
+    #         return n * self.factorialNumbers(n-1)
+    
+    # Brute force
+    
         # temp = []
         # fact = 1
         # for i in range(1,n+1):
-        #     fact = fact * i
-        #     if fact < n:
+        #     for j in range(1,i+1):
+        #         fact = fact * j
+        #     if fact <= n:
         #         temp.append(fact)
+        #         fact= 1
         # return temp
+        
+        # temp = []
+    # 	fact = 1
+    # 	for i in range(1,n+1):
+    # 	    fact = fact * i
+    # 	    temp.append(fact)
+    	
+    # 	result = []  
+    # 	for i in temp:
+    # 	    if i < n+1:
+    # 	        result.append(i)
+    # 	return result
+        
         
         temp = []
         fact = 1
         i = 1
+    
         while fact <= n:
             temp.append(fact)
-            i = i + 1
-            fact = fact * i
-            
-            
+            i += 1
+            fact *= i
+    
         return temp
             
-            
-    
 
 
-
-
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    t = int(input())
-    for _ in range(t):
-        N = int(input())
-        ob = Solution()
-        ans = ob.factorialNumbers(N)
-        for i in ans:
-            print(i, end=" ")
-        print()
-        print("~")
-
-# } Driver Code Ends
