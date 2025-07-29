390fc87a8f9e6c737a471da5684201f84e42aae1
Rajan Kumar <101178067+iamrajank@users.noreply.github.com>
2025-07-29 23:27:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Armstrong Numbers/armstrong-numbers.py
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.py
@@ -3,32 +3,15 @@
 class Solution:
     def armstrongNumber (self, n):
         # code here 
-        len_n = len(str(n))
-        arm_num = 0
+        result = 0
+        power = len(str(n))
         n_copy = n
         while n > 0:
             digit = n % 10
-            arm_num = arm_num + digit ** len_n
+            result = result + digit ** power
             n = n // 10
-        if n_copy == arm_num:
+        if n_copy == result:
             return True
-        return False
-            
-
-
-#{ 
- # Driver Code Starts
-# Initial Template for Python 3
-if __name__ == '__main__':
-    t = int(input())
-    for _ in range(t):
-        n = input()
-        n = int(n)
-        ob = Solution()
-        flag = ob.armstrongNumber(n)
-        if flag:
-            print("true")
         else:
-            print("false")
-        print("~")
-# } Driver Code Ends
+            return False
+        
