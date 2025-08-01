b55bd6747237d617f9d158fe4dbbfd6669abe7ed
Rajan Kumar <101178067+iamrajank@users.noreply.github.com>
2025-08-01 22:54:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Palindromic Array/palindromic-array.py
+++ Difficulty: Basic/Palindromic Array/palindromic-array.py
@@ -12,17 +12,3 @@ def isPalinArray(arr):
 
 
 
-#{ 
- # Driver Code Starts
-# Driver Program
-if __name__ == '__main__':
-    t = int(input())
-    for i in range(t):
-        arr = list(map(int, input().strip().split()))
-        if isPalinArray(arr):
-            print("true")
-        else:
-            print("false")
-        print("~")
-
-# } Driver Code Ends
